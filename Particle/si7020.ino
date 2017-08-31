5c7999ee47557df7380f0ec4ecdb8b951c7ac4f9
zaphod-42 <trey@tomadoh.com>
2017-08-31 15:46:37 -0500

    Create si7020.ino

---
+++ Particle/si7020.ino
@@ -0,0 +1,35 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "SI7020.h"
+
+SI7020 sensor;
+
+void setup() {
+    sensor.scale="f";
+    sensor.init();
+    Particle.variable("temperature", sensor.temperature);
+    Particle.variable("humidity", sensor.humidity);
+    Particle.function("getTemp", getTemp);
+}
+
+int last_read = 0;
+void loop() {
+    int now = millis();
+    if(now-last_read > 2000){
+        last_read = now;
+        sensor.takeReading();
+    }
+    
+}
+
+int getTemp(String scale){
+    if(scale.equalsIgnoreCase("f")){
+        return sensor.getTempF();
+    }
+    if(scale.equalsIgnoreCase("c")){
+        return sensor.getTempC();
+    }
+    if(scale.equalsIgnoreCase("k")){
+        return sensor.getTempK();
+    }
+    return 0;
+}
