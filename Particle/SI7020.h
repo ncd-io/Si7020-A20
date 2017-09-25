89e6707f7a5dd12d8d09aa93f4b05c1c38633fa5
zaphod-42 <trey@tomadoh.com>
2017-09-25 15:39:21 -0500

    Update SI7020.h

--- Particle/SI7020.h
+++ Particle/SI7020.h
@@ -1,24 +1,60 @@
 #include "spark_wiring_i2c.h"
 #include "spark_wiring_constants.h"
 
+//Configuration Register
+#define SI7020_CONFIG_REG_READ 0xE7
+#define SI7020_CONFIG_REG_WRITE 0xE6
+
+#define SI7020_RES_RH12_TMP14 0x00 // 22.8
+#define SI7020_RES_RH8_TMP12 0x01 // 6.9
+#define SI7020_RES_RH10_TMP_13 0x80 // 10.7
+#define SI7020_RES_RH11_TMP11 0x81 // 9.4
+
+#define SI7020_HEATER_ENABLED 0x04
+
+#define SI7020_CONFIG_RSVD 0x3A
+
+#define SI7020_HOLD_MODE 0x00
+#define SI7020_NO_HOLD_MODE 0x10
+
+#define SI7020_HUMIDITY_REG 0xE5
+#define SI7020_TEMP_REG 0xE3
+#define SI7020_TEMP_AFTER_RH 0xE0
+
+#define SI7020_RESET 0xFE
+
+//Heater setting, allows value between 0 and 15
+#define SI7020_HEATER_WRITE 0x51
+#define SI7020_HEATER_READ 0x11
+
+#define SI7020_HEATER_RESERVED 0xF0
+
+#define TEMP_CELSIUS 0x00
+#define TEMP_FAHRENHEIT 0x01
+#define TEMP_KELVIN 0x02
+
 class SI7020{
 public:
     //Constructor
-    SI7020(void);
-    //Set Address.  Indicate status of jumpers on board.  Send 0 for not installed, send 1 for installed
     int address=0x40;
     void init();
     
-    int temp_code;
-    String scale="c";
+    int hold_mode = SI7020_HOLD_MODE;
+    int reading_waiting = 0;
+    int getDelay();
+    
+    int resolution = SI7020_RES_RH12_TMP14;
+    int heater_status = 0;
+    int heater_level = 0;
+    
+    int scale = TEMP_CELSIUS;
     double humidity;
     double temperature;
     
     void takeReading();
-    
-    double getTempK();
-    double getTempC();
-    double getTempF();
-    
-    void setHeater(bool on);
+private:
+    void begin();
+    void writeByte(int reg, int data);
+    int readByte(int reg);
+    void readBuffer(int reg, int *buff, int length);
 };
